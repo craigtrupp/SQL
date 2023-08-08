b0afe68b736231f80dfb77c63da1ab333311b487
Craig Rupp <craigtrupp8@gmail.com>
2023-08-07 20:45:55 -0500

    Sql file consolidation of queries from markdown for section A

--- Sql_Challenges/7_BalancedTreeClothing/balanced_tree_case.sql
+++ Sql_Challenges/7_BalancedTreeClothing/balanced_tree_case.sql
@@ -30,3 +30,20 @@ FROM product_counts AS pc
 INNER JOIN balanced_tree.product_prices AS pp 
   ON pc.prod_id = pp.product_id
 ORDER BY product_rev_pre_disc DESC;
+
+-- (Also works for 2 ... lol)
+SELECT
+  -- Will multiply each row and simply take the sum of all rows quanity * price at the end
+  SUM(qty * price) AS total_revenue,
+  CAST(SUM(qty * price) AS money) AS str_total_revenue
+FROM balanced_tree.sales;
+
+-- 3
+SELECT
+  -- subtract base price by the discounted price of a product rounded to two decimals
+  SUM(qty * ROUND(price - (price * discount/100::NUMERIC), 2)) AS total_amount_post_discount,
+  CAST(SUM(qty * ROUND(price - (price * discount/100::NUMERIC), 2)) AS money) AS total_post_discount_$,
+  -- and now we will simply take the total pre-discount and minus total-after discount for eact sale
+  SUM(qty * price) - SUM(qty * ROUND(price - (price * discount/100::NUMERIC), 2)) AS total_discount_amount,
+  CAST(SUM(qty * price) - SUM(qty * ROUND(price - (price * discount/100::NUMERIC), 2)) AS money) AS discount_amount_$
+FROM balanced_tree.sales;

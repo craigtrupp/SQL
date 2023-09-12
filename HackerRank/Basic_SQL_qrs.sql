50c805543d55d8bd90e84c4c826638f58043b032
Craig Rupp <craigtrupp8@gmail.com>
2023-09-12 12:28:51 -0500

    Ranking function for largest and shortest city names in unioned output

--- HackerRank/Basic_SQL_qrs.sql
+++ HackerRank/Basic_SQL_qrs.sql
@@ -8,3 +8,31 @@ SELECT
 SELECT 
     total_cities - unique_cities
 FROM diff_values;
+
+
+-- Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths 
+-- (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that 
+-- comes first when ordered alphabetically.
+WITH longest_city_name_alpha AS (
+SELECT 
+    city, LENGTH(city) AS city_alpha_length,
+    RANK() OVER(
+        ORDER BY LENGTH(city) DESC, city
+    ) AS longest_alpha_rankings
+FROM STATION
+),
+shortest_city_name_alpha AS (
+SELECT 
+    city, LENGTH(city) AS city_alpha_length,
+    RANK() OVER(
+        ORDER BY LENGTH(city), city
+    ) AS shortest_alpha_rankings
+FROM STATION
+)
+SELECT city, city_alpha_length
+FROM longest_city_name_alpha
+WHERE longest_alpha_rankings = 1
+UNION
+SELECT city, city_alpha_length
+FROM shortest_city_name_alpha
+WHERE shortest_alpha_rankings = 1;

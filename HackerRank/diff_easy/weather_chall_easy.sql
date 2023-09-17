b0c675bc6a19fb3d3d8b2efd5299d431a094e0a4
Craig Rupp <craigtrupp8@gmail.com>
2023-09-17 13:51:47 -0500

    Finishing off final weather observation challenges - derived (subqueries) for conditional weather coordinate searches

--- HackerRank/diff_easy/weather_chall_easy.sql
+++ HackerRank/diff_easy/weather_chall_easy.sql
@@ -139,3 +139,88 @@ FROM distinct_cities
 WHERE 
     LEFT(LOWER(city), 1) NOT in ('a', 'e', 'i', 'o', 'u')
     AND RIGHT(LOWER(city), 1) NOT in ('a', 'e','i','o','u');
+
+
+-- Weather Observation 2 : SQL Agg (Easy)
+-- Query the following two values from the STATION table:
+-- The sum of all values in LAT_N rounded to a scale of  decimal places.
+-- The sum of all values in LONG_W rounded to a scale of  decimal places.
+/*
+Enter your query here.
+*/
+SELECT
+    ROUND(SUM(lat_n), 2) AS lat_n_round_2,
+    ROUND(SUM(long_w), 2) AS long_w_round_2
+FROM STATION;
+
+
+-- Weahter Observation 13 : SQL Agg (Easy)
+-- Query the sum of Northern Latitudes (LAT_N) from STATION having values 
+-- greater 38.7880 than and less than 137.2345. Truncate your answer to 4 decimal places.
+/*
+Enter your query here.
+*/
+SELECT
+    ROUND(SUM(lat_n), 4) as lat_n_gt_38
+FROM STATION
+WHERE lat_n BETWEEN 38.7880 AND 137.2345;
+
+
+-- Weather Observation 14 : SQL Agg (Easy)
+-- Query the greatest value of the Northern Latitudes (LAT_N) from STATION 
+-- that is less than 137.2345. Truncate your answer to 4 decimal places.
+/*
+Enter your query here.
+*/
+SELECT
+    ROUND(MAX(lat_n), 4) AS max_lat_n_lt_137
+FROM STATION
+WHERE lat_n < 137.2345;
+
+
+-- Weather Observation 15 : SQL Agg (Easy)
+-- Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) 
+-- in STATION that is less than 137.2345. Round your answer to 4 decimal places.
+/*
+Enter your query here.
+*/
+SELECT
+    ROUND(long_w, 4) AS cond_long_w
+FROM (
+    SELECT *
+    FROM STATION
+    WHERE lat_n < 137.2345
+    ORDER BY lat_n DESC
+    LIMIT 1
+) AS derived_table;
+
+
+-- Weather Observation 16 : SQL Agg (Easy)
+-- Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780.
+-- Round your answer to 4 decimal places.
+/*
+Enter your query here.
+*/
+SELECT
+    ROUND(MIN(lat_n), 4) AS min_lat_condition
+FROM STATION
+WHERE lat_n > 38.7780;
+
+
+-- Weather Observation 17: SQL Agg (Easy)
+-- Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in 
+-- STATION is greater than 38.7780. Round your answer to 4 decimal places.
+/*
+Enter your query here.
+*/
+SELECT
+    * 
+FROM
+(
+    SELECT
+        ROUND(long_w, 4) AS long_w_condition
+    FROM STATION
+    WHERE lat_n > 38.7780
+    ORDER BY lat_n
+    LIMIT 1
+) AS derived_output;

0db4820bb2121da5833ec2b4067fb516d7577985
Craig Rupp <craigtrupp8@gmail.com>
2023-10-02 11:44:51 -0500

    Debugging through unsuccessful challenge submissions, ranking criteria has ~20 duplicate values of wands with same age, power, and minimum galleoon amount. The expected output only wants one of these wand types so need to figure out what/how that final criteria is working

--- HackerRank/diff_medium/third_medium_set.sql
+++ HackerRank/diff_medium/third_medium_set.sql
@@ -183,6 +183,118 @@ LIMIT 5
 
 -- So if we check this against the expected output, we can now see that a join would need to be done
 -- from the output of the derived query to get the final features from the two tables to create the expected output
+/*
+Enter your query here.
+*/
+-- SELECT VERSION(); -- 5.7.27-0ubuntu0.18.04.1 (No Ranking Function Allowed)
+SELECT
+    wds.id AS wand_id, mgws.wand_age AS wand_age, 
+    mgws.min_galleons AS min_galleons, mgws.wand_power AS wand_power
+FROM 
+    (
+    SELECT
+        wand_age, wand_power, MIN(galleons_needed) AS min_galleons
+    FROM
+        (
+        SELECT
+            w.id AS wand_id, wp.age AS wand_age, w.coins_needed AS galleons_needed, 
+            w.power AS wand_power, wp.code AS wp_code, wp.is_evil
+        FROM Wands AS w 
+        INNER JOIN Wands_Property AS wp
+            USING(code)
+        WHERE wp.is_evil != 1
+        ORDER BY w.power DESC, wp.age DESC
+        ) AS jwa
+    GROUP BY wand_age, wand_power
+    ORDER BY wand_power DESC, wand_age DESC
+    ) AS mgws
+INNER JOIN Wands AS wds
+    ON mgws.wand_power = wds.power AND mgws.min_galleons = wds.coins_needed
+ORDER BY wand_power DESC, wand_age DESC;
 
+
+    
+
+-- Output
+-- 1038 496 4789 10 
+-- 774 494 9439 10 
+-- 1130 494 9439 10 
+-- 1315 492 4126 10 
+-- 9 491 7345 10 
+-- 858 483 4352 10 
+-- 1164 481 9831 10 
+-- 1288 464 4952 10 
+-- 861 462 8302 10 
+-- 412 455 5625 10 
+
+-- Expected
+-- 1038 496 4789 10 
+-- 1130 494 9439 10 
+-- 1315 492 4126 10 
+-- 9 491 7345 10 
+-- 858 483 4352 10 
+-- 1164 481 9831 10 
+-- 1288 464 4952 10 
+-- 861 462 8302 10 
+-- 412 455 5625 10 
+-- 996 451 8884 10 
+
+--- ** Well ... now we have a wand with the same age, power, and shared low galleon_price so we're not quite getting the expected output
+-- In the criteria I'm not seeing a described tie-breaker for this scenario but the output is expecting the higher id
+-- There's about ... 700+ rows of wands, how many tie-breaker scenarios are there (wands of same age, power, and minimum galleon score?)
+SELECT 
+wand_age, min_galleons, wand_power, COUNT(*) AS unique_f_wnd_output_criteria
+FROM (
+    SELECT
+        wds.id AS wand_id, mgws.wand_age AS wand_age, 
+        mgws.min_galleons AS min_galleons, mgws.wand_power AS wand_power
+    FROM 
+        (
+        SELECT
+            wand_age, wand_power, MIN(galleons_needed) AS min_galleons
+        FROM
+            (
+            SELECT
+                w.id AS wand_id, wp.age AS wand_age, w.coins_needed AS galleons_needed, 
+                w.power AS wand_power, wp.code AS wp_code, wp.is_evil
+            FROM Wands AS w 
+            INNER JOIN Wands_Property AS wp
+                USING(code)
+            WHERE wp.is_evil != 1
+            ORDER BY w.power DESC, wp.age DESC
+            ) AS jwa
+        GROUP BY wand_age, wand_power
+        ORDER BY wand_power DESC, wand_age DESC
+        ) AS mgws
+    INNER JOIN Wands AS wds
+        ON mgws.wand_power = wds.power AND mgws.min_galleons = wds.coins_needed
+    ORDER BY wand_power DESC, wand_age DESC
+) AS final_wand_query_distinct_check
+GROUP BY wand_age, min_galleons, wand_power
+ORDER BY unique_f_wnd_output_criteria DESC;
+
+-- Duplicate Rows for Current Criteria (wand_search)
+-- 290 7761 7 2 
+-- 208 5422 6 2 
+-- 438 3185 2 2 
+-- 494 9439 10 2 
+-- 481 615 9 2 
+-- 292 6740 7 2 
+-- 381 6740 7 2 
+-- 240 9556 9 2 
+-- 211 2060 7 2 
+-- 443 5967 1 2 
+-- 464 9556 9 2 
+-- 164 3480 2 2 
+-- 483 556 2 2 
+-- 414 5869 9 2 
+-- 492 2782 2 2 
+-- 335 4263 6 2 
+-- 292 9241 1 2 
+-- 318 520 10 2 
+-- 176 653 1 2 
+-- 301 3754 6 2 
+-- 344 3185 2 2 
+-- 343 653 1 2 
 
 
